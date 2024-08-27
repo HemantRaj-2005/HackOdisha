69dd8b1de3312c289034a6b227889c290f6d24c2
Hemant Raj <coder.hemantraj05@gmail.com>
2024-08-28 00:05:48 +0530

    added routes, created pages and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
