7bf7f328ce4b6ff3fbe2f9844dfde25de0335b9f
Hemant Raj <coder.hemantraj05@gmail.com>
2024-08-28 01:35:41 +0530

    added a middleware and a function to handle errors

--- server/index.js
+++ server/index.js
@@ -20,5 +20,15 @@ app.listen(3000, () => {
     console.log("server is running on port 3000");
 })
 
-app.use('/server/user', userRoutes)
-app.use("/api/auth", authRoutes)
+app.use('/server/user', userRoutes);
+app.use("/server/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      statusCode,
+    });
+  });
